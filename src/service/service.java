9e25819c706a0cf21ec95cad970cdfac89b11a8a
winhtuan <winhtuan@gmail.com>
2024-10-21 16:29:17 +0700

    up

--- src/service/service.java
+++ src/service/service.java
@@ -2,16 +2,16 @@ package service;
 
 import java.time.format.DateTimeFormatter;
 
-public interface service<T> {
+public interface Service<T> {
+
+    void display();
 
     T findByID(String id);
-
-    public void display();
     
     final String USER_REGEX = "CUS-\\d{4}";
     final String COURSE_REGEX = "COR-\\d{4}";
     final String COACH_REGEX = "COA-\\d{4}";
-    
+
     final String TEXT_REGEX = "^([A-Za-z]+(?:\\s[A-Za-z]+)*)$";
     final String SSN_REGEX = "^\\d{12}$";
     final String PHONE_REGEX = "^0\\d{9}$";
